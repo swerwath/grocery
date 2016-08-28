20f4fd674bea7affa6ad669d78621bbaa4fd6d42
swerwath <swerwath@berkeley.edu>
2016-08-28 11:41:51 -0700

    Fixed syntax error

--- db/migrate/20160828181125_default_price0.rb
+++ db/migrate/20160828181125_default_price0.rb
@@ -1,6 +1,5 @@
 class CreateItems < ActiveRecord::Migration
   def change
     change_column :items, :price, :integer, :default => 0, :null => false
-    end
   end
 end
--- db/migrate/20160828181128_default_bought.rb
+++ db/migrate/20160828181128_default_bought.rb
@@ -1,6 +1,5 @@
 class CreateItems < ActiveRecord::Migration
   def change
     change_column :items, :bought, :boolean, :default => false, :null => false
-    end
   end
 end
