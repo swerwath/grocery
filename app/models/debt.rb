a6ae85221f07a9d95cdb86e814ccba44611d177c
swerwath <swerwath@berkeley.edu>
2016-09-01 19:21:43 -0700

    Added debt relations

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,2 +1,6 @@
 class Debt < ActiveRecord::Base
+
+  belongs_to :debtor, :class_name => 'User'
+  belongs_to :creditor, :class_name => 'User'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ActiveRecord::Base
   has_many :bought_items, :through => :item_buys, :source => :item
   has_many :requested_items, :through => :item_requests, :source => :item
 
+  has_many :debts, :foreign_key => 'debtor_id', :class_name => 'Debt'
+  has_many :credits, :foreign_key => 'creditor_id', :class_name => 'Debt'
+
 end
